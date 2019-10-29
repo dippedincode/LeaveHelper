e1312b048d24dbefcc6e086b1a0bf2e860250778
Diptis Halder <diptis.halder@gmail.com>
2019-10-29 06:02:35 +0000

    Class which defines the data in a booking
    A booking is a descriptor (short note for the leave reason), start date and end date.

---
+++ Booking.java
@@ -0,0 +1,27 @@
+import org.joda.time.DateTime;
+import org.joda.time.LocalDate;
+
+class Booking {
+    private String descriptor;
+    private LocalDate startDate;
+    private LocalDate endDate;
+
+    public Booking(String descriptor, LocalDate startDate, LocalDate endDate) {
+        this.descriptor = descriptor;
+        this.startDate = startDate;
+        this.endDate = endDate;
+    }
+
+    public String getDescriptor() {
+        return descriptor;
+    }
+
+    public LocalDate getStartDate() {
+        return startDate;
+    }
+
+    public LocalDate getEndDate() {
+        return endDate;
+    }
+
+}
